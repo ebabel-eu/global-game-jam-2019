883d09321337ea07b0189b4331e3b781ac688916
Thomas Amar <hello@ebabel.eu>
2019-01-26 23:54:18 +0100

    Play game is trying to click on items.

--- src/modules/play-game.js
+++ src/modules/play-game.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import * as C from '../constants';
-import distance from '../utils/distance';
 
 /**
  * `PlayGame`
@@ -15,9 +14,50 @@ class PlayGame extends Phaser.Scene {
       lastTap: null,
 
       // Pick a random number of safe and dangerous items to hide in the room.
-      safeItems: Phaser.Math.Between(1, 3),
-      dangerousItems: Phaser.Math.Between(1, 5),
+      maxGoodItems: Phaser.Math.Between(1, 3),
+      maxBadItems: Phaser.Math.Between(1, 5),
+
+      goodItems: [],
+      badItems: [],
     };
+  }
+
+  // Return a randomly selected good or bad hidden item.
+  getGoodOrBadSecret() {
+    let goodPossible = (this.EG.goodItems.length < this.EG.maxGoodItems);
+    let badPossible = (this.EG.badItems.length < this.EG.maxBadItems);
+
+    // Neither good nor bad are possible.
+    if (!goodPossible && !badPossible) {
+      return null;
+    }
+
+    const good = ['chocolate-heart', 'teddy-bear', 'turtle'];
+    const bad = ['bomb', 'heart', 'land-mine', 'sword', 'tommy-gun'];
+
+    // Only good is possible.
+    if (goodPossible) {
+      const goodChoice = good[Phaser.Math.Between(0, good.length - 1)];
+      this.EG.goodItems.push(goodChoice);
+      return goodChoice;
+    }
+
+    //  Only bad is possible.
+    if (badPossible) {
+      const badChoice = bad[Phaser.Math.Between(0, bad.length - 1)];
+      this.EG.badItems.push(badChoice);
+      return badChoice;
+    }
+
+    return null;
+  }
+
+  onObjectClicked(pointer, gameObject) {
+    if (!window.isDoubleTap) {
+      return;
+    }
+
+    console.log(gameObject.texture.key);
   }
 
   create() {
@@ -25,10 +65,23 @@ class PlayGame extends Phaser.Scene {
 
     this.input.on('pointerdown', this.onTap, this);
 
-    // First room.
+    // Furnish the first room.
     window.EG.rooms[0].items.map(s => {
-      const sprite = this.add.sprite(s.x, s.y, s.id)
+      const secret = this.getGoodOrBadSecret();
+
+      // If the sprite can hide a secret item, place that secret item first.
+      if (s.secret && secret) {
+        const secretItem = this.add.sprite(s.x, s.y, secret);
+        secretItem.setInteractive();
+        this.input.on('gameobjectdown', this.onObjectClicked);
+      }
+
+      // Add the main item to the scene, unless this item can be switched.
+      const sprite = (s.switched && secret) ?
+        this.add.sprite(s.x, s.y, secret)
+        : this.add.sprite(s.x, s.y, s.id);
       
+      // Make the item draggable if the current rooms says it is.
       if (s.drag) {
         sprite.setInteractive();
         this.input.setDraggable(sprite);
@@ -53,6 +106,8 @@ class PlayGame extends Phaser.Scene {
     const isDoubleTap = (this.EG.lastTap && this.EG.lastTap.x === pointer.x && this.EG.lastTap.y === pointer.y
       && (Date.now() - this.EG.lastTap.tapTime) < 250);
 
+    window.isDoubleTap = isDoubleTap;
+
     if (isDoubleTap) {
       this.addStamp(pointer.x, pointer.y);
     }
@@ -61,17 +116,7 @@ class PlayGame extends Phaser.Scene {
   }
 
   addStamp(x, y) {
-    // Determine distance from center of the scene.
-    const _distance = distance(x, y, C.gameWidth / 2, C.gameHeight / 2);
-    let scale = _distance / 400;
-
-    // todo: handle exceptions based on object laid in the scene.
-
-    // todo: define the back wall of the room as an "object"
-    scale = (scale < 0.3) ? 0.3 : scale;
-
-    // todo: show the correct frame of the spritesheet based on location in the room.
-    this.add.tileSprite(x, y, 100, 100, 'stamps').setScale(scale);
+    this.add.tileSprite(x, y, 100, 100, 'stamps');
   }
 
   // Game loop function that gets called continuously unless a game over.
